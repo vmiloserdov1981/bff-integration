6cc28eb49cdf942ad2b0ca43eb07b0f8f0df3b09
Alexey Kochnev <alexey.kochnev@quantumsoft.ru>
2023-05-22 08:41:40 +0000

    Обновленные локаторы боковой панели для testuser

--- core/pages/blocks/sidebar.py
+++ core/pages/blocks/sidebar.py
@@ -10,8 +10,28 @@ class SidebarMenu(BasePage):
         super().__init__(page=page.page, page_url=page.page_url)
 
     @property
+    def org_tree_button(self) -> Locator:
+        return self.locator('//a[@class[contains(.,"Navigation_Link")]][@href[contains(.,"/orgTree")]]')
+
+    @property
+    def mark_editor_button(self) -> Locator:
+        return self.locator('//a[@class[contains(.,"Navigation_Link")]][@href[contains(.,"/markEditor")]]')
+
+    @property
+    def graphics_button(self) -> Locator:
+        return self.locator('//div[@class[contains(.,"Navigation_Link")]]/span[contains(.,Icon_icon)]').first
+
+    @property
+    def settings_button(self) -> Locator:
+        return self.locator('//a[@class[contains(.,"Navigation_Link")]][@href[contains(.,"/settings")]]')
+
+    @property
     def logout_button(self) -> Locator:
         return self.locator('//div[@class[contains(.,"Navigation_Link")]]/span[contains(.,Icon_icon)]').last
 
     def check_specific_locators(self) -> None:
+        expect(self.org_tree_button).to_be_visible(timeout=Timeouts.DEFAULT)
+        expect(self.mark_editor_button).to_be_visible(timeout=Timeouts.DEFAULT)
+        expect(self.graphics_button).to_be_visible(timeout=Timeouts.DEFAULT)
+        expect(self.settings_button).to_be_visible(timeout=Timeouts.DEFAULT)
         expect(self.logout_button).to_be_visible(timeout=Timeouts.DEFAULT)
